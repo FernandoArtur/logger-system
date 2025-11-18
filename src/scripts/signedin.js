6d87f49b115c54ec135c8fa5a33b6e7f665028b6
Fernando Artur <nandinhoartury@gmail.com>
2025-11-18 20:55:35 -0300

    Update signedin.js

--- src/scripts/signedin.js
+++ src/scripts/signedin.js
@@ -1,5 +1,6 @@
 const congratulationsMessage = document.querySelector('#congratulations');
 const currentUser = sessionStorage.getItem('name');
 
+congratulationsMessage.style.textAlign = 'center';
 
-congratulationsMessage.textContent = `Hi, ${currentUser} you have successfully logged in!`;
+congratulationsMessage.textContent = `Hi ${currentUser}, you have successfully logged in!`;
